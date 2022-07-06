d528563667911fb62738580c4f6460241f7627a3
Padrition <padrition@gmail.com>
2022-07-06 03:44:51 +0200

    Instruction Fx65

--- src/processor.rs
+++ src/processor.rs
@@ -1,22 +1,22 @@
 use rand::Rng;
 
-const CHIP8_FONT: [u8;80] = [
-    0xF0, 0x90, 0x90, 0x90, 0xF0,		// 0
-	0x20, 0x60, 0x20, 0x20, 0x70,		// 1
-	0xF0, 0x10, 0xF0, 0x80, 0xF0,		// 2
-	0xF0, 0x10, 0xF0, 0x10, 0xF0,		// 3
-	0x90, 0x90, 0xF0, 0x10, 0x10,		// 4
-	0xF0, 0x80, 0xF0, 0x10, 0xF0,		// 5
-	0xF0, 0x80, 0xF0, 0x90, 0xF0,		// 6
-	0xF0, 0x10, 0x20, 0x40, 0x40,		// 7
-	0xF0, 0x90, 0xF0, 0x90, 0xF0,		// 8
-	0xF0, 0x90, 0xF0, 0x10, 0xF0,		// 9
-	0xF0, 0x90, 0xF0, 0x90, 0x90,		// A
-	0xE0, 0x90, 0xE0, 0x90, 0xE0,		// B
-	0xF0, 0x80, 0x80, 0x80, 0xF0,		// C
-	0xE0, 0x90, 0x90, 0x90, 0xE0,		// D
-	0xF0, 0x80, 0xF0, 0x80, 0xF0,		// E
-	0xF0, 0x80, 0xF0, 0x80, 0x80		// F
+const CHIP8_FONT: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 ];
 
 #[derive(Debug)]
@@ -87,13 +87,13 @@ impl Cpu {
                 (0x8, _, _, 0x4) => self.add_y_to_x(x, y),
                 (0x8, _, _, 0x5) => self.sub_y_from_x(x, y),
                 (0x8, _, _, 0x6) => self.right_shift_x(x),
-                (0x8, _, _, 0x7) => self.sub_x_from_y(x,y),
+                (0x8, _, _, 0x7) => self.sub_x_from_y(x, y),
                 (0x8, _, _, 0xE) => self.left_shift_x(x),
-                (0x9, _, _, 0x0) => self.comparte_x_y(x,y),
+                (0x9, _, _, 0x0) => self.comparte_x_y(x, y),
                 (0xA, _, _, _) => self.store_addres(nnn),
                 (0xB, _, _, _) => self.jump_to_addr_and_v0(nnn),
                 (0xC, _, _, _) => self.store_rand_to_x(x, kk),
-                (0xD, _, _, _) => self.draw_a_sprite(x,y,d),
+                (0xD, _, _, _) => self.draw_a_sprite(x, y, d),
                 (0xE, _, 0x9, 0xE) => self.skip_if_pressed(x),
                 (0xE, _, 0xA, 0x1) => self.skip_if_not_pressed(x),
                 (0xF, _, 0x0, 0x7) => self.store_delayt_to_x(x),
@@ -104,84 +104,90 @@ impl Cpu {
                 (0xF, _, 0x2, 0x9) => self.set_i_to_sprite_addr(x),
                 (0xF, _, 0x3, 0x3) => self.bcd_from_x_to_i(x),
                 (0xF, _, 0x5, 0x5) => self.store_registers_to_memory(x),
+                (0xF, _, 0x6, 0x5) => self.read_memory_to_registers(x),
                 _ => todo!("TODO: {:0x}", opcode),
             }
 
             self.program_counter_increase();
         }
     }
-    fn store_registers_to_memory(&mut self, x: u8){
-        for i in (0..x){
+    fn read_memory_to_registers(&mut self, x: u8) {
+        for j in (0..x) {
+            self.register[j as usize] = self.memory[(self.i + j as u16) as usize];
+        }
+    }
+    fn store_registers_to_memory(&mut self, x: u8) {
+        for i in (0..x) {
             self.memory[(self.i + i as u16) as usize] = self.register[i as usize];
         }
     }
-    fn bcd_from_x_to_i(&mut self, x: u8){
+    fn bcd_from_x_to_i(&mut self, x: u8) {
         let decimal = self.register[x as usize];
         let i1 = decimal / 100; //maybe there is a better way of converting to bcd, but I came up with this my self. Idk if I shloud be proud or ashamed.
-        let i2 = (decimal - i1 *100) / 10;
-        let i3 = (decimal - i1 *100) - i2 * 10;
+        let i2 = (decimal - i1 * 100) / 10;
+        let i3 = (decimal - i1 * 100) - i2 * 10;
 
         self.memory[self.i as usize] = i1;
         self.memory[(self.i + 1) as usize] = i2;
         self.memory[(self.i + 2) as usize] = i3;
     }
-    fn set_i_to_sprite_addr(&mut self, x: u8){
+    fn set_i_to_sprite_addr(&mut self, x: u8) {
         let vx = self.register[x as usize];
         let sprite_length = 5;
         self.i = vx as u16 * sprite_length;
     }
-    fn add_x_to_i(&mut self, x: u8){
+    fn add_x_to_i(&mut self, x: u8) {
         self.i = self.i + x as u16;
     }
-    fn set_soundt(&mut self, x: u8){
+    fn set_soundt(&mut self, x: u8) {
         let vx = self.register[x as usize];
         self.sound_timer = vx;
     }
-    fn set_delayt(&mut self, x: u8){
+    fn set_delayt(&mut self, x: u8) {
         let vx = self.register[x as usize];
         self.delay_timer = vx;
     }
-    fn wait_for_press(&mut self, x: u8){
+    fn wait_for_press(&mut self, x: u8) {
         todo!("Wait for press");
     }
-    fn store_delayt_to_x(&mut self, x: u8){
+    fn store_delayt_to_x(&mut self, x: u8) {
         self.register[x as usize] = self.delay_timer;
     }
-    fn skip_if_not_pressed(&mut self, x: u8){
+    fn skip_if_not_pressed(&mut self, x: u8) {
         todo!("SKIP not");
     }
-    fn skip_if_pressed(&mut self, x: u8){
+    fn skip_if_pressed(&mut self, x: u8) {
         todo!("SKIP");
     }
-    fn draw_a_sprite(&mut self, x: u8, y: u8, n: u8){
+    fn draw_a_sprite(&mut self, x: u8, y: u8, n: u8) {
         todo!("DRAW!");
     }
-    fn store_rand_to_x(&mut self, x: u8, kk: u8){
+    fn store_rand_to_x(&mut self, x: u8, kk: u8) {
         let mut rng = rand::thread_rng();
         let mut rand_num: u8 = rng.gen_range(0..255);
         rand_num = rand_num & kk;
         self.register[x as usize] = rand_num;
     }
-    fn jump_to_addr_and_v0(&mut self, nnn: u16){
+    fn jump_to_addr_and_v0(&mut self, nnn: u16) {
         let v0 = self.register[0] as u16;
         self.jump_to_subroutine(nnn + v0);
     }
-    fn store_addres(&mut self, nnn: u16){
+    fn store_addres(&mut self, nnn: u16) {
         self.i = nnn;
     }
-    fn comparte_x_y(&mut self, x: u8, y: u8){
+    fn comparte_x_y(&mut self, x: u8, y: u8) {
         let vx = self.register[x as usize];
         let vy = self.register[y as usize];
 
-        if vx != vy{
+        if vx != vy {
             self.program_counter_increase();
         }
     }
-    fn left_shift_x(&mut self, x: u8){
+    fn left_shift_x(&mut self, x: u8) {
         self.register[0xF] = self.register[x as usize] & 0b0000_0001;
-        self.register[x as usize] <<= 1; 
+        self.register[x as usize] <<= 1;
     }
-    fn sub_x_from_y(&mut self, x: u8, y: u8){
+    fn sub_x_from_y(&mut self, x: u8, y: u8) {
         let vx = self.register[x as usize];
         let vy = self.register[y as usize];
 
@@ -293,14 +299,26 @@ impl Cpu {
 mod test {
     use super::*;
     #[test]
+    fn read_memory_to_registers_test() {
+        let mut cpu = Cpu::new();
+        cpu.i = 0x300;
+        for j in (0..cpu.register.len()) {
+            cpu.memory[0x300 + j] = j as u8;
+        }
+        cpu.read_memory_to_registers(16);
+        for j in (0..cpu.register.len()) {
+            assert_eq!(cpu.register[j], j as u8);
+        }
+    }
+    #[test]
     fn store_registers_to_memory_test() {
         let mut cpu = Cpu::new();
         cpu.i = 0x300;
-        for j in (0..cpu.register.len()){
+        for j in (0..cpu.register.len()) {
             cpu.register[j] = j as u8;
         }
         cpu.store_registers_to_memory(16);
-        for j in (0..cpu.register.len()){
+        for j in (0..cpu.register.len()) {
             assert_eq!(cpu.register[j], cpu.memory[0x300 + j]);
         }
     }
@@ -324,7 +342,7 @@ mod test {
     #[test]
     fn store_rand_to_x_test() {
         let mut cpu = Cpu::new();
-        for i in (0..255){
+        for i in (0..255) {
             cpu.store_rand_to_x(0, i);
             let range = 0..255;
             assert!(range.contains(&cpu.register[0]));
