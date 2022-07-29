236c15a36f2c6fbb6699d33290f22305bdae66eb
Padrition <padrition@gmail.com>
2022-07-29 12:06:24 +0200

    Added methods for switching state of emulator struct

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,13 +1,16 @@
+#[derive(PartialEq)]
 pub struct Emulator{
     pub emulator_state: EmulatorState,
     pub emulator_choice: EmulatorChoice,
 }
 
+#[derive(PartialEq)]
 pub enum EmulatorState{
     InGame,
     InMenu,
 }
 
+#[derive(PartialEq)]
 pub enum EmulatorChoice{
     LoadRom,
     Quit,
@@ -18,6 +21,20 @@ impl Emulator{
         Emulator{
             emulator_state: EmulatorState::InMenu,
             emulator_choice: EmulatorChoice::LoadRom,
+        }
+    }
+    pub fn switch_state(&mut self){
+        self.emulator_state = if self.emulator_state == EmulatorState::InGame{
+            EmulatorState::InMenu
+        }else{
+            EmulatorState::InGame
+        }
+    }
+    pub fn switch_choice(&mut self){
+        self.emulator_choice = if self.emulator_choice == EmulatorChoice::LoadRom{
+            EmulatorChoice::Quit
+        }else{
+            EmulatorChoice::LoadRom
         }
     }
 }
