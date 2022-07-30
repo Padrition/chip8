6df52af9849e9bf15901e9217d5f60d80e090528
Padrition <padrition@gmail.com>
2022-07-30 15:35:48 +0200

    Added methods for choosing a game

--- src/cartridge_reader.rs
+++ src/cartridge_reader.rs
@@ -4,6 +4,7 @@ pub struct Cartridge {
     pub game_paths: Vec<String>,
     pub rom: Vec<u8>,
     choosen_game: String,
+    game_index: usize,
 }
 
 impl Cartridge {
@@ -12,10 +13,11 @@ impl Cartridge {
             game_paths: Vec::<String>::new(),
             rom: Vec::<u8>::new(),
             choosen_game: String::new(), 
+            game_index: 0
         };
         let path = read_dir("assets/").unwrap();
         cartridge.load_game_paths(path);
-        cartridge.choosen_game = cartridge.game_paths[0].clone();
+        cartridge.choosen_game = cartridge.game_paths[cartridge.game_index].clone();
         cartridge.game_to_rom();
         cartridge
     }
@@ -41,5 +43,27 @@ impl Cartridge {
         let game = game.trim_end_matches(".ch8");
         let game = game.trim_start_matches("assets/");
         game
+    }
+    pub fn next_game(&mut self){
+        let game_count = self.game_paths.len();
+        let game_array_len = game_count - 1;
+        self.game_index = if self.game_index == game_array_len{
+            0
+        }else{
+            self.game_index + 1
+        };
+        let index = self.game_index;
+        self.choosen_game = self.game_paths[index].clone();
+    }
+    pub fn previous_game(&mut self){
+        let game_count = self.game_paths.len();
+        let game_array_len = game_count - 1;
+        self.game_index = if self.game_index == 0{
+            game_array_len
+        }else{
+            self.game_index - 1
+        };
+        let index = self.game_index;
+        self.choosen_game = self.game_paths[index].clone();
     }
 }
